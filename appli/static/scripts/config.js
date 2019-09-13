0c000a394a55ee3de68da05833a4d14d7ac7aaa5
marcram99 <marcram99@gmail.com>
2019-09-13 18:12:20 +0200

    work on config.html js

--- appli/static/scripts/config.js
+++ appli/static/scripts/config.js
@@ -1,11 +1,28 @@
 var mod_start = document.getElementById('i_mod_st')
 var h_start = document.getElementById('p_start')
+var actif = 'badge badge-secondary mr-2'
+var inactif = 'badge badge-light mr-2'
+var lundi = document.getElementById('lundi')
+var mardi = document.getElementById('mardi')
+var mercredi = document.getElementById('mercredi')
+var jeudi = document.getElementById('jeudi')
+var vendredi = document.getElementById('vendredi')
+var samedi = document.getElementById('samedi')
+var dimanche = document.getElementById('dimanche')
+var week = [lundi, mardi, mercredi, jeudi, vendredi, samedi, dimanche]
 
+week.forEach(function(jour){
+    jour.onclick = function(){
+        event.preventDefault()
+        if(jour.className == actif){
+            jour.className = inactif
+        }
+        else if(jour.className == inactif){
+            jour.className = actif
+        } 
+    }
+})
 
-mod_start.onclick = function(){
-    event.preventDefault()
-    mod_start.style = "color:red;"
-}
 
 /*
 <input type="time" id="p_start" class="btn btn-secondary btn-sm mr-2"></input>
